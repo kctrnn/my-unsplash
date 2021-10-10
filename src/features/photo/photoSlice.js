29054e714f73391cd65549b2999de0e1f6cb8036
Kim Chan <kctrnn@gmail.com>
2021-10-10 22:36:25 +0700

    Setup photoSlice

---
+++ src/features/photo/photoSlice.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import photoApi from 'api/photoApi';
+
+export const fetchPhotoList = createAsyncThunk('photo/fetchPhotoList', async (payload) => {
+  const data = await photoApi.get(payload);
+  return data;
+});
+
+const photoSlice = createSlice({
+  name: 'photo',
+  initialState: {
+    list: [],
+    filter: {},
+  },
+
+  reducers: {
+    setFilter(state, action) {
+      state.filter = action.payload;
+    },
+  },
+
+  extraReducers: {
+    [fetchPhotoList.fulfilled]: (state, action) => {
+      console.log(action);
+      //   state.list = action.payload;
+    },
+  },
+});
+
+// Actions
+export const { setFilter } = photoSlice.actions;
+
+// Selector
+export const selectPhotoList = (state) => state.photo.list;
+export const selectPhotoFilter = (state) => state.photo.filter;
+
+// Reducer
+const photoReducer = photoSlice.reducer;
+export default photoReducer;
